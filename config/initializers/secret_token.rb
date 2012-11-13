3e17891540eae9df659b10198bb2847dd78dd596
badnazo <florian.lhomeau@gmail.com>
2012-11-13 04:47:29 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsiteUnilan::Application.config.secret_token = '09e2f1dae16cd466f8be26ab7452c244229d8f570e8871289e46358952a1dd20df1ed995edbd33ec3b6ca0dccb7c8565b1ae3280adf5c43e9dc271e9dfa36a25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WebsiteUnilan::Application.config.session_store :cookie_store, key: '_WebsiteUnilan_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WebsiteUnilan::Application.config.session_store :active_record_store
